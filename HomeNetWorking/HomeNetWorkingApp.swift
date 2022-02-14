66677c0ace965f21c297224d059d6e3b3271f44b
iMtRo <maxmudovilyos75@gmail.com>
2022-02-14 18:22:33 +0500

    Initial Commit

---
+++ HomeNetWorking/HomeNetWorkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeNetWorkingApp.swift
+//  HomeNetWorking
+//
+//  Created by user on 14/02/22.
+//
+
+import SwiftUI
+
+@main
+struct HomeNetWorkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
